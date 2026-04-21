be8d151404694926d4b41e9d3a8b266c001ca90f
miniaoumiae <miniaoumiae@noreply.codeberg.org>
2026-04-21 09:55:36 +0200

    feat: add gte registers

--- src/cop2.zig
+++ src/cop2.zig
@@ -1,7 +1,106 @@
 pub const Cop2 = struct {
     const Self = @This();
 
-    // GTE has 32 Data registers and 32 Control registers
+    pub const DataReg = enum(u5) {
+        // Vector 0, 1, 2
+        vxy0 = 0,
+        vz0 = 1,
+        vxy1 = 2,
+        vz1 = 3,
+        vxy2 = 4,
+        vz2 = 5,
+
+        rgbc = 6, // Color/code value
+        otz = 7, // Average Z value (for Ordering Table)
+
+        ir0 = 8, // 16bit Accumulator (Interpolate)
+        ir1 = 9,
+        ir2 = 10,
+        ir3 = 11, // 16bit Accumulator (Vector)
+
+        // Screen XY-coordinate FIFO
+        sxy0 = 12,
+        sxy1 = 13,
+        sxy2 = 14,
+        sxyp = 15,
+
+        // Screen Z-coordinate FIFO
+        sz0 = 16,
+        sz1 = 17,
+        sz2 = 18,
+        sz3 = 19,
+
+        // Color CRGB-code/color FIFO
+        rgb0 = 20,
+        rgb1 = 21,
+        rgb2 = 22,
+
+        res1 = 23, // Prohibited / Reserved
+
+        // 32bit Maths Accumulators
+        mac0 = 24,
+        mac1 = 25,
+        mac2 = 26,
+        mac3 = 27,
+
+        // Convert RGB Color
+        irgb = 28,
+        orgb = 29,
+
+        // Count Leading-Zeroes/Ones
+        lzcs = 30,
+        lzcr = 31,
+    };
+
+    pub const CtrlReg = enum(u5) {
+        // Rotation matrix (3x3)
+        rt11_rt12 = 0,
+        rt13_rt21 = 1,
+        rt22_rt23 = 2,
+        rt31_rt32 = 3,
+        rt33 = 4,
+
+        // Translation vector (X,Y,Z)
+        trx = 5,
+        try_ = 6,
+        trz = 7, // Note: 'try' is a reserved keyword in Zig!
+
+        // Light source matrix (3x3)
+        l11_l12 = 8,
+        l13_l21 = 9,
+        l22_l23 = 10,
+        l31_l32 = 11,
+        l33 = 12,
+
+        // Background color (R,G,B)
+        rbk = 13,
+        gbk = 14,
+        bbk = 15,
+
+        // Light color matrix source (3x3)
+        lr1_lr2 = 16,
+        lr3_lg1 = 17,
+        lg2_lg3 = 18,
+        lb1_lb2 = 19,
+        lb3 = 20,
+
+        // Far color (R,G,B)
+        rfc = 21,
+        gfc = 22,
+        bfc = 23,
+
+        // Screen offset (X,Y)
+        ofx = 24,
+        ofy = 25,
+
+        h = 26, // Projection plane distance
+        dqa = 27, // Depth queuing parameter A (coeff)
+        dqb = 28, // Depth queuing parameter B (offset)
+        zsf3 = 29,
+        zsf4 = 30, // Average Z scale factors
+        flag = 31, // Returns any calculation errors
+    };
+
     data_regs: [32]u32 = [_]u32{0} ** 32,
     ctrl_regs: [32]u32 = [_]u32{0} ** 32,
 
@@ -10,20 +109,34 @@ pub const Cop2 = struct {
     }
 
     // Move From/To Data Registers (MFC2 / MTC2)
-    pub fn readData(self: *const Self, index: u5) u32 {
-        return self.data_regs[index];
+    pub fn readData(self: *const Self, index: anytype) u32 {
+        return self.data_regs[getDataIdx(index)];
     }
 
-    pub fn writeData(self: *Self, index: u5, value: u32) void {
-        self.data_regs[index] = value;
+    pub fn writeData(self: *Self, index: anytype, value: u32) void {
+        self.data_regs[getDataIdx(index)] = value;
     }
 
     // Move From/To Control Registers (CFC2 / CTC2)
-    pub fn readCtrl(self: *const Self, index: u5) u32 {
-        return self.ctrl_regs[index];
+    pub fn readCtrl(self: *const Self, index: anytype) u32 {
+        return self.ctrl_regs[getCtrlIdx(index)];
     }
 
-    pub fn writeCtrl(self: *Self, index: u5, value: u32) void {
-        self.ctrl_regs[index] = value;
+    pub fn writeCtrl(self: *Self, index: anytype, value: u32) void {
+        self.ctrl_regs[getCtrlIdx(index)] = value;
+    }
+
+    inline fn getDataIdx(index: anytype) u5 {
+        return switch (@typeInfo(@TypeOf(index))) {
+            .int, .comptime_int => @as(u5, @truncate(index)),
+            else => @intFromEnum(@as(DataReg, index)),
+        };
+    }
+
+    inline fn getCtrlIdx(index: anytype) u5 {
+        return switch (@typeInfo(@TypeOf(index))) {
+            .int, .comptime_int => @as(u5, @truncate(index)),
+            else => @intFromEnum(@as(CtrlReg, index)),
+        };
     }
 };
